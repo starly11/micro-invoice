5bb6d881630803c013f619ca08727492727c9f07
starly-g <zynoochic.starly@gmail.com>
2026-02-23 13:19:48 +0500

    confirm add proxy in passport.js

--- server/src/configs/passport.js
+++ server/src/configs/passport.js
@@ -7,7 +7,8 @@ dotenv.config()
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // 1. Check if user exists (By Google ID OR Email)
